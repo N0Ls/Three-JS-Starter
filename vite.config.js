efdbc010172aa471c3d132e2f9a9adda2151bffd
Nils Landrodie <nilsland31@gmail.com>
2023-09-11 14:39:53 +0200

    Packages update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import glsl from 'vite-plugin-glsl'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import glsl from "vite-plugin-glsl";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+const isCodeSandbox = "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
 
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
+    root: "src/",
+    publicDir: "../static/",
+    base: "./",
     server:
     {
         host: true,
@@ -14,7 +14,7 @@ export default {
     },
     build:
     {
-        outDir: '../dist',
+        outDir: "../dist",
         emptyOutDir: true,
         sourcemap: true
     },
@@ -23,4 +23,4 @@ export default {
         glsl(),
         basicSsl()
     ]
-}
+};
